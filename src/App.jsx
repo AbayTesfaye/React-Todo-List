e025e7bba8e5a142cb4d4941af9cc479fbdf8ca6
AbayTesfaye <abaytesfaye02@gmail.com>
2024-10-06 16:52:21 -0700

    Use local Storage to store todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 import "./index.css";
 
 function App() {
-  // State to manage the list of todos
+  // State to manage the list of todos and the current todo being edited
   const [todos, setTodos] = useState([]);
-  const [todoValue, setTodoValue] = useState();
+  const [todoValue, setTodoValue] = useState("");
+
+  // Function to persist the data to localStorage
+  function persistData(newList) {
+    localStorage.setItem("todos", JSON.stringify({ todos: newList }));
+  }
 
   // Function to add a new todo
   function handleAddTodo(newTodo) {
-    if (newTodo.trim()) {
-      setTodos([...todos, newTodo]); // Add the new todo to the array
-    }
+    const newTodoList = [...todos, newTodo];
+    persistData(newTodoList); // Use the newTodoList
+    setTodos(newTodoList);
   }
 
+  // Function to delete a todo
   function handleDeleteTodo(index) {
-    const newTodo = todos.filter((todo, todoId) => {
-      return todoId !== index;
-    });
-    setTodos(newTodo);
+    const newTodoList = todos.filter((todo, todoId) => todoId !== index);
+    persistData(newTodoList); // Use the newTodoList
+    setTodos(newTodoList);
   }
 
+  // Function to edit a todo
   function handleEditTodo(index) {
     const valueToBeEdited = todos[index];
-    setTodoValue(valueToBeEdited);
-    handleDeleteTodo(index);
+    setTodoValue(valueToBeEdited); // Set the value to be edited in the input
+    handleDeleteTodo(index); // Remove the todo from the list temporarily
   }
+
+  // Load todos from localStorage on the initial render
+  useEffect(() => {
+    const localTodos = localStorage.getItem("todos");
+    if (localTodos) {
+      const parsedTodos = JSON.parse(localTodos).todos;
+      setTodos(parsedTodos);
+    }
+  }, []);
 
   return (
     <>
